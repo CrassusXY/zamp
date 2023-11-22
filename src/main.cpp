23b89b8967af3d2781d5983af53c722bb87033c3
crassusx <259278@student.pwr.edu.pl>
2023-11-22 09:51:12 +0100

    init on remote

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,80 @@
 #include <iostream>
 #include <dlfcn.h>
 #include <cassert>
+#include <map>
+#include <memory>
 #include "AbstractInterp4Command.hh"
 
 using namespace std;
 
+class LibInterface {
+    void * _pLibHnd = nullptr;
+    AbstractInterp4Command *(*_pCreateCmd)(void) = nullptr;
+  public: 
+    ~LibInterface() {if(_pLibHnd) dlclose(_pLibHnd); }
+    bool init(const char * sFileName);
+    AbstractInterp4Command *CreateCmd() {return _pCreateCmd(); }
+};
 
-int main()
+bool LibInterface::init(const char * sFileName)
 {
-  void *pLibHnd_Move = dlopen("libInterp4Move.so",RTLD_LAZY);
-  AbstractInterp4Command *(*pCreateCmd_Move)(void);
-  void *pFun;
-
-  if (!pLibHnd_Move) {
-    cerr << "!!! Brak biblioteki: Interp4Move.so" << endl;
-    return 1;
+  _pLibHnd = dlopen(sFileName,RTLD_LAZY);
+  if (!_pLibHnd) {
+    cerr << "!!! Brak biblioteki: " << sFileName << endl;
+    return false;
   }
 
-
-  pFun = dlsym(pLibHnd_Move,"CreateCmd");
+  void *pFun = dlsym(_pLibHnd,"CreateCmd");
+  
   if (!pFun) {
     cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
     return 1;
   }
-  pCreateCmd_Move = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
+  
+  _pCreateCmd = reinterpret_cast<AbstractInterp4Command* (*)(void)>(pFun);
+
+  return true;
+}
 
 
-  AbstractInterp4Command *pCmd = pCreateCmd_Move();
+class Set4LibInterfaces {
+  private:
+    map<string, shared_ptr<LibInterface>> mSet;
+  public:
+    bool init();
+    shared_ptr<LibInterface> get_interface(string lib_name);
+};
 
+bool Set4LibInterfaces::init() 
+{
+  string libs[] = {"libInterp4Set", "libInterp4Move"};
+  string lib_path;
+  for (size_t i = 0; i < sizeof(libs) / sizeof(libs[0]); i++)
+  {
+    shared_ptr<LibInterface> tmp(new LibInterface());
+    lib_path = libs[i] + ".so";
+    tmp->init(lib_path.c_str());
+    mSet.emplace(libs[i], tmp);
+  }
+  return true;
+}
+
+shared_ptr<LibInterface> Set4LibInterfaces::get_interface(string lib_name)
+{
+  return mSet[lib_name];
+}
+
+int main()
+{
+  Set4LibInterfaces dupa;
+  dupa.init();
+
+  shared_ptr<LibInterface> tmp = dupa.get_interface("libInterp4Move");
+  AbstractInterp4Command *pCmd = tmp->CreateCmd();
+  
+  tmp = dupa.get_interface("libInterp4Set");
+  AbstractInterp4Command *pCmdA = tmp->CreateCmd();
+ 
   cout << endl;
   cout << pCmd->GetCmdName() << endl;
   cout << endl;
@@ -35,8 +82,15 @@ int main()
   cout << endl;
   pCmd->PrintCmd();
   cout << endl;
-  
-  delete pCmd;
 
-  dlclose(pLibHnd_Move);
+  cout << endl;
+  cout << pCmdA->GetCmdName() << endl;
+  cout << endl;
+  pCmdA->PrintSyntax();
+  cout << endl;
+  pCmdA->PrintCmd();
+  cout << endl;
+
+  delete pCmd;
+  delete pCmdA;
 }
